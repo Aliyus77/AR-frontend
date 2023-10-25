33da5dabbe112424a978fd41338a07b7615db56c
Angel Honorio Rodriguez <angel.ho.rodriguez@gmail.com>
2023-10-25 16:58:51 -0300

    menu hamburguesa en mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import React from 'react'
-import { FaRegSun, FaMoon } from "react-icons/fa6";
+import { FaMoon, FaBars } from "react-icons/fa6";
+import { HiOutlineSun } from "react-icons/hi2";
+
 
 const Header = () => {
   const [theme, setTheme] = useState('light')
@@ -32,22 +34,53 @@ const Header = () => {
   const handleChangeTheme = () => {
     setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
   }
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div>      
       <nav className='flex justify-between bg-slate-400 dark:bg-slate-600 bg-opacity-75 dark:bg-opacity-80 backdrop:blur-md dark:backdrop:blur-lg p-4 fixed w-full top-0 z-50'>
-        <div className='cursor-pointer align-middle' >
-          <FaMoon id='moon' onClick={handleChangeTheme} />
-          <FaRegSun id='sun' onClick={handleChangeTheme} />
+        <div className='text-xl ml-4 flex gap-8 ' >
+          <FaMoon className='cursor-pointer' id='moon' onClick={handleChangeTheme} />
+          <HiOutlineSun className='cursor-pointer' id='sun' onClick={handleChangeTheme} />
+          <FaBars 
+            className='cursor-pointer text-xl z-60 sm:hidden' 
+            onClick={() => setOpen(!open)}  
+          />          
         </div>
-        <div className='' >
-          <ul className='flex flex-row gap-2 sm:gap-6 cursor-pointer font-bold'>
-            <li><a href='#about'>Acerca de mi</a></li>
-            <li><a href='#service'>Servicios</a></li>
-            <li><a href='#works'>Trabajos</a></li>
-            <li><a href='#formation'>Formación</a></li>
-            <li><a href='#contact'>Contacto</a></li>
+        <div className='hidden sm:block' >
+          <ul className='flex flex-row gap-2 sm:gap-6 font-bold'>
+            <li className='cursor-pointer'><a href='#about'>Acerca de mi</a></li>
+            <li className='cursor-pointer'><a href='#service'>Servicios</a></li>
+            <li className='cursor-pointer'><a href='#works'>Trabajos</a></li>
+            <li className='cursor-pointer'><a href='#formation'>Formación</a></li>
+            <li className='cursor-pointer'><a href='#contact'>Contacto</a></li>
           </ul>
         </div>
+
+        {/* SIDEBAR */}
+          <div className={`${!open && 'hidden'} bg-slate-600/50 min-h-screen w-full fixed top-14 left-0 right-0 backdrop-blur-sm`}>
+          </div>
+          <div className={`${open ? 'w-2/3' : 'w-0' } bg-slate-600 min-h-screen fixed top-14 left-0 sm:hidden transition-all duration-300`}>        
+            <ul className={`${!open && 'hidden'} flex flex-col gap-2 sm:hidden font-semibold text-2xl text-center text-white py-8`}>
+              <li 
+                className='py-3 mb-2 cursor-pointer  hover:text-slate-300'
+                onClick={() => setOpen(false)}
+              ><a href='#about'>Acerca de mi</a></li>
+              <li 
+                className='py-3 mb-2 cursor-pointer hover:text-slate-300'
+                onClick={() => setOpen(false)}><a href='#service'>Servicios</a></li>
+              <li 
+                className='py-3 mb-2 cursor-pointer hover:text-slate-300'
+                onClick={() => setOpen(false)}><a href='#works'>Trabajos</a></li>
+              <li 
+                className='py-3 mb-2 cursor-pointer hover:text-slate-300'
+                onClick={() => setOpen(false)}><a href='#formation'>Formación</a></li>
+              <li 
+                className='py-3 mb-2 cursor-pointer hover:text-slate-300'
+                onClick={() => setOpen(false)}><a href='#contact'>Contacto</a></li>
+            </ul>            
+          </div>
       </nav>
     </div>
   )
---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// const Sidebar = () => {
+//   return (
+    
+//   )
+// }
+// // Acerca de mi
+// // Servicios
+// // Trabajos
+// // Formación
+// // Contacto
+// // Acerca de mi
+// export default Sidebar
