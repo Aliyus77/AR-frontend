a0290a70269d54e734dbcafc10f5ca27982d58ed
Angel Honorio Rodriguez <angel.ho.rodriguez@gmail.com>
2023-10-25 11:59:12 -0300

    linkedin actualizado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='bg-slate-400 dark:bg-slate-600 flex h-10 items-center'>
       <aside className='flex gap-2 absolute pl-3'>
-        <a target='_blank' href='https://www.linkedin.com/in/angel-honorio-rodriguez-b74076b5/'><FaLinkedin className='dark:text-slate-400'/></a>
+        <a target='_blank' href='https://www.linkedin.com/in/angel-h-rodriguez-dev/'><FaLinkedin className='dark:text-slate-400'/></a>
         <a target='_blank' href='https://www.facebook.com/aliyuslgi'><FaFacebook className='dark:text-slate-400' /></a>
         <a target='_blank' href='https://www.instagram.com/aliyus.lgi/'><FaInstagram className='dark:text-slate-400' /></a>
         <a target='_blank' href='https://twitter.com/ALIYUS_LGI'><FaSquareXTwitter className='dark:text-slate-400' /></a>
