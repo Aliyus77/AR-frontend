e761e82438e0ea0fae6466cd1632561b5fb07bd5
Angel Honorio Rodriguez <angel.ho.rodriguez@gmail.com>
2023-10-24 22:41:04 -0300

    arreglo responsive en services

--- src/components/Service/index.jsx
+++ src/components/Service/index.jsx
@@ -6,8 +6,8 @@ const Service = () => {
     <section id='service' className='flex w-8/12 m-auto pt-20 pb-5'>
       <div className='grid grid-cols-1 gap-8'>
         {servicesData.map((service, index) => (
-          <div key={index} className='flex gap-3 dark:text-slate-300'>
-            <img className='w-52' src={service.imageSrc} alt={service.title} />
+          <div key={index} className='lg:flex lg:text-start gap-3 dark:text-slate-300 text-center'>
+            <img className='w-52 mx-auto' src={service.imageSrc} alt={service.title} />
             <aside className='dark:text-slate-300'>
               <h3 className='font-semibold pb-2'>{service.title}</h3>
               <p>{service.description}</p>
