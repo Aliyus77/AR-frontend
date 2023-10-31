e1461778cad2d33e38732d4568e45def1c9b38b7
Angel Honorio Rodriguez <angel.ho.rodriguez@gmail.com>
2023-10-30 22:16:21 -0300

    se agrego xmark para cierre de menu responsive y se trabajo sobre el blur del menu desplegable y ajustes de dimensiones

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import React from 'react'
-import { FaMoon, FaBars } from "react-icons/fa6";
+import { FaMoon, FaBars, FaX } from "react-icons/fa6";
 import { HiOutlineSun } from "react-icons/hi2";
 
 
 const Header = () => {
   const [theme, setTheme] = useState('light')
+  const handleChangeTheme = () => {
+    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
+  }
+  const [open, setOpen] = useState(false)
+  
+  // AÑADIR CLASE AL HTML PARA MODO NOCHE
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -14,6 +20,8 @@ const Header = () => {
       document.querySelector('html').classList.remove('dark')
     }
   }, [theme])
+
+  // TOGGLE DE ICONO MODO DIA Y NOCHE
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -31,21 +39,41 @@ const Header = () => {
     }
   }, [theme])
 
-  const handleChangeTheme = () => {
-    setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light')
-  }
+  // TOGGLE DE ICONO MENU DESPLEGABLE
 
-  const [open, setOpen] = useState(false)
+  useEffect(() => {
+    if (open === true) {
+      document.getElementById('xMark').classList.remove('hidden')      
+    } else {
+      document.getElementById('xMark').classList.add('hidden')
+    }
+  })
+
+  useEffect(() => {
+    if (open === false) {
+      document.getElementById('bars').classList.remove('hidden')      
+    } else {
+      document.getElementById('bars').classList.add('hidden')
+    }
+  })
 
   return (
-    <div>      
-      <nav className='flex justify-between bg-slate-400 dark:bg-slate-600 bg-opacity-75 dark:bg-opacity-80 backdrop:blur-md dark:backdrop:blur-lg p-4 fixed w-full top-0 z-50'>
+    <div> 
+      <div className={`${!open && 'hidden'} bg-slate-600/50 min-h-screen sm:hidden w-full fixed top-14 left-0 right-0 backdrop-blur-sm`}>
+      </div>     
+      <nav className='flex justify-between bg-slate-400/60 dark:bg-slate-600/70 backdrop-blur-sm p-4 fixed w-full h-14 top-0 z-50'>
         <div className='text-xl ml-4 flex gap-8 ' >
           <FaMoon className='cursor-pointer' id='moon' onClick={handleChangeTheme} />
           <HiOutlineSun className='cursor-pointer' id='sun' onClick={handleChangeTheme} />
-          <FaBars 
+          <FaBars
+            id='bars' 
             className='cursor-pointer text-xl z-60 sm:hidden' 
             onClick={() => setOpen(!open)}  
+          />
+          <FaX
+            id='xMark'
+            className='cursor-pointer z-60 sm:hidden' 
+            onClick={() => setOpen(!open)} 
           />          
         </div>
         <div className='hidden sm:block' >
@@ -56,11 +84,7 @@ const Header = () => {
             <li className='cursor-pointer'><a href='#formation'>Formación</a></li>
             <li className='cursor-pointer'><a href='#contact'>Contacto</a></li>
           </ul>
-        </div>
-
-        {/* SIDEBAR */}
-          <div className={`${!open && 'hidden'} bg-slate-600/50 min-h-screen w-full fixed top-14 left-0 right-0 backdrop-blur-sm`}>
-          </div>
+        </div>         
           <div className={`${open ? 'w-2/3' : 'w-0' } bg-slate-600 min-h-screen fixed top-14 left-0 sm:hidden transition-all duration-300`}>        
             <ul className={`${!open && 'hidden'} flex flex-col gap-2 sm:hidden font-semibold text-2xl text-center text-white py-8`}>
               <li 
